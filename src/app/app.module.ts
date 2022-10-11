d11a4c6780eb9dcc752418eaca6a6cae9160386c
Nelly Guepnang <56829563+Nelly-98@users.noreply.github.com>
2022-10-11 11:09:59 +0200

    Calculatrice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculatriceComponent } from './calculatrice/calculatrice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatriceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
